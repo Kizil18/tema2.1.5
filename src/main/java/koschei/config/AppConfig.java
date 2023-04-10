8551954769327c43e2d93d3b35ce1281d57e1ea5
Ilya Kizilbashev <ikizil018@gmail.com>
2023-04-10 22:40:37 +0300

    10/04

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,5 +1,7 @@
 package koschei.config;
 
+import koschei.models.Deth8;
+import koschei.models.Egg6;
 import koschei.models.Island2;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +11,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
+    @Bean
+    public static Egg6 getEgg(Deth8 deth8) {
+        return new Egg6(deth8);
+    }
 
     @Bean
     public static Island2 getIsland(Wood3 wood) {
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,14 @@
 package koschei.models;
 
 public class Egg6 {
+    private Deth8 deth8;
+
+    public Egg6(Deth8 deth8) {
+        this.deth8 = deth8;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + deth8.toString();
     }
 }
